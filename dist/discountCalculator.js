549e8e47e77f1ef42930afa67ccd58ec50df7094
waseem-sk-dev <shaikhrgp@gmail.com>
2025-05-14 10:15:07 +0530

    Initial commit

---
+++ dist/discountCalculator.js
@@ -0,0 +1,25 @@
+"use strict";
+function calculateTotal(orderItems) {
+    let totalAmount = 0;
+    for (let i = 0; i < orderItems.length; i++) {
+        const subTotal = orderItems[i].quantity * orderItems[i].price;
+        let discount = 0;
+        if (orderItems[i].quantity >= 10) {
+            discount = 0.2; // 20% discount
+        }
+        else if (orderItems[i].quantity >= 5) {
+            discount = 0.1; // 10% discount
+        }
+        const discountedTotal = subTotal - subTotal * discount;
+        totalAmount += discountedTotal;
+    }
+    return `Order processed. Total amount after discounts: ₹${totalAmount}`;
+}
+// Sample input
+const orderItems = [
+    { name: "Laptop", quantity: 3, price: 50000 },
+    { name: "Mouse", quantity: 10, price: 500 },
+    { name: "Keyboard", quantity: 6, price: 1500 },
+];
+// Output
+console.log(calculateTotal(orderItems));
